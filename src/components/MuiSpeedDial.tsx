9bfbe6c223d8945e67768c3785dc5054b0023be9
Rosemary-Oti <otirosemary58@gmail.com>
2024-10-18 17:45:37 +0100

    completed speedDial

--- src/components/MuiSpeedDial.tsx
+++ src/components/MuiSpeedDial.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { SpeedDial, SpeedDialAction, SpeedDialIcon } from "@mui/material"
+import CopyIcon from '@mui/icons-material/CopyAllOutlined'
+import PrintIcon from '@mui/icons-material/Print'
+import ShareIcon from '@mui/icons-material/Share'
+import EditIcon from '@mui/icons-material/Edit'
+
 
 export const MuiSpeedDial = () => {
   return (
-    <div>MuiSpeedDial</div>
+    <SpeedDial 
+      ariaLabel='Speed Dial Navigation'
+      sx={{position: 'absolute', bottom: 16, right: 16 }}
+      icon={<SpeedDialIcon openIcon={<EditIcon />} />}
+    >
+      <SpeedDialAction icon={<CopyIcon />} tooltipTitle='copy' tooltipOpen/>
+      <SpeedDialAction icon={<PrintIcon />} tooltipTitle='print' tooltipOpen/>
+      <SpeedDialAction icon={<ShareIcon />} tooltipTitle='share' tooltipOpen/>
+    </SpeedDial>
   )
 }
